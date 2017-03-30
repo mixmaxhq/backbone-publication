9a19e844f802e5495a12088cd02db8b5cced826b
Chuy Martinez <chuy@mixmax.com>
2017-03-30 13:15:51 -0700

    Add tests.

---
+++ spec/PublicationModelSpec.js
@@ -0,0 +1,48 @@
+import PublicationModel from '../src/PublicationModel';
+
+describe('PublicationModel', () => {
+  describe('#set', () => {
+    it('should trigger "change" for a set call in object style', (done) => {
+      const model = new PublicationModel();
+
+      model.on('change', (model, changes) => {
+        // `change` will trigger on when creating the new model.
+        if (_.isEmpty(changes)) return;
+
+        expect(changes.foo).toEqual(true);
+        done();
+      });
+
+      model.set({foo: true});
+    });
+
+    it('should trigger "change" for a set call in (key, value) style', (done) => {
+      const model = new PublicationModel();
+
+      model.on('change', (model, changes) => {
+        // `change` will trigger on when creating the new model.
+        if (_.isEmpty(changes)) return;
+
+        expect(changes.foo).toEqual(false);
+        done();
+      });
+
+      model.set('foo', false);
+    });
+
+    it('should trigger a change for a nested attribute change', (done) => {
+      const model = new PublicationModel({ foo: { bar: true } });
+
+      model.on('change', (model, changes) => {
+        expect(changes.foo.bar).toEqual(false);
+        done();
+      });
+
+      model.set({
+        foo: {
+          bar: false
+        }
+      });
+    });
+  });
+});
